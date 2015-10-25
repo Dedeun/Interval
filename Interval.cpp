4e8ce99acd12fa5d8e43645a35cdc0e3377ce1a7
Dedeun <deden@free.fr>
2015-10-25 14:49:17 +0100

    Second version

--- Interval.cpp
+++ Interval.cpp
@@ -3,25 +3,38 @@
 #include <algorithm>
 
 //using namespace dd;
-
+// *******************************************************************************
+// * class IntervalSingle
+// *******************************************************************************
+// Constructor (with contract verification)
 IntervalSingle::IntervalSingle(int limMin, int limMax) :
             m_min{limMin}, m_max{limMax}
 {
     assert ((m_min <= m_max)&&"Inversion of the limit");
 };
 
+// Intersection of 2 intervals
 bool IntervalSingle::intersection (IntervalSingle const& inter)
 {
+    // 4 cases to be evaluated
+    // Case 1: Inter insides *this
+    // Case 2: Inter on right of *this
+    // case 3: Inter on left of *this
+    // Case 4: Inter and *this have no common point
     if (belong(inter.m_min)) {
         m_min = inter.m_min;
         if (belong(inter.m_max)) {
+            // it is case 1
             m_max = inter.m_max;
         } // end if
+        // it is case 2
         return true;
     } else if (belong(inter.m_max)) {
+        // it is case 3
         m_max = inter.m_max;
         return true;
     } else if ((m_min >= inter.m_max) || (inter.m_min >= m_max)) {
+        // it is case 4
         m_min = 0;
         m_max = 0;
         return (false);
@@ -31,9 +44,14 @@ bool IntervalSingle::intersection (IntervalSingle const& inter)
 
 bool IntervalSingle::reunion (IntervalSingle const& inter)
 {
+    // 2 cases to be evaluated
+    // Case 1: there is no common points between the two interval ==> the result is not one interval!
+    // Case 2: There is common point between the two intervals ==> the result is one interval.
     if ((m_min > inter.m_max) || (inter.m_min > m_max)) {
+        // It is case 1
         return false;
     } else {
+        // it is case 2
         if (m_min > inter.m_min) {
             m_min = inter.m_min;
         } // end if
@@ -64,6 +82,10 @@ bool IntervalSingle::isEmpty() const
     return (m_min == m_max);
 }
 
+
+// *******************************************************************************
+// * Free function: Is it a good idea ?
+// *******************************************************************************
 IntervalSingle intersection (IntervalSingle const& inter1, IntervalSingle const& inter2)
 {
     IntervalSingle inter{inter1};
@@ -71,27 +93,32 @@ IntervalSingle intersection (IntervalSingle const& inter1, IntervalSingle const&
     return (inter);
 }
 
-
+// *******************************************************************************
+// * class IntervalMultiple
+// *******************************************************************************
+// Constructor (with contract verification)
 IntervalMultiple::IntervalMultiple (int limMin, int limMax)
 {
     m_inter.clear();
     IntervalSingle inter{limMin, limMax};
     m_inter.push_back(inter);
 }
+
 void IntervalMultiple::intersection (IntervalSingle const& inter)
 {
-    // pour chaque interval du multi interval, faire l'intersection avec le nouveau interval
-
+    // For each element of the vectors, realize the intersection with the new interval.
+    //      --> What "algorithm" can be used ? (For_each does not modify the source container)
     for (size_t i =0; i < m_inter.size(); i++) {
         m_inter[i].intersection (inter);
     } //end for
-    // reduire le multi interval.
 
+    // reduce the interval multiple (remove the empty interval)
     std::vector<IntervalSingle>::iterator it;
     auto f = std::bind(&IntervalMultiple::isEmpty, this, std::placeholders::_1);
     it = std::remove_if (m_inter.begin(), m_inter.end(), f);
     m_inter.resize( std::distance(m_inter.begin(),it) );
 
+    // if the multiple interval is empty, add a null interval (refer definition of multiple interval)
     if (m_inter.size() == 0) {
         IntervalSingle tmp{0,0};
         reunion (tmp);
@@ -101,13 +128,16 @@ void IntervalMultiple::intersection (IntervalSingle const& inter)
 
 void IntervalMultiple::reunion (IntervalSingle const& inter)
 {
-    // pour chaque interval du multi interval, faire la reunion avec le nouveau interval
+    // For each interval of the multiple interval, do the reunion with the interval
+    //      --> What "algorithm" can be used ? (For_each does not modify the source container, it is not possible to return a boolean)
     bool unionDone {false};
     for (size_t i =0; i < m_inter.size(); i++) {
         unionDone |= m_inter[i].reunion (inter);
     } //end for
+    // If one or more union have been done
     std::vector<IntervalSingle>::iterator it;
     if (unionDone) {
+        // reduce the multiple interval (loop until there is no more intersection between interval)
         do {
             unionDone = false;
             for (size_t i =0; i < m_inter.size()-1; i++) {
@@ -120,12 +150,14 @@ void IntervalMultiple::reunion (IntervalSingle const& inter)
             m_inter.resize( std::distance(m_inter.begin(),it) );
         } while (unionDone);
     } else {
-    // Si pas de reunion, on l'ajoute à la fin, et on fait une mise par ordre croissant.
+    // if no union have been done
+    // add the new interval to the multiple interval
         m_inter.push_back(inter);
         std::sort(m_inter.begin(), m_inter.end());
     }
 }
 
+// Function only used for the "remove-if" algorithm.
 bool IntervalMultiple::isEmpty (IntervalSingle const& inter) const
 {
     return inter.isEmpty();
